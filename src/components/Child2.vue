df95de12510a2e29e04442a4bd9d70b14fbeb09c
Seungwon Go <seungwon.go@gmail.com>
2021-07-06 17:28:37 +0900

    Create Child2.vue

---
+++ src/components/Child2.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <button @click="sendDataToParent">자식에서 부모로 데이터 전달</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "",
+  components: {},
+  data() {
+    return {
+      msg: "자식 컴포넌트에서 보내는 메시지",
+    };
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {
+    sendDataToParent() {
+      this.$emit("send-message", this.msg);
+    },
+  },
+};
+</script>
